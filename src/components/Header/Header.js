444ea9e479410d920dc86963a7e8186e2d5ade5d
Mathieu Bonhommeau <mat.bonhommeau85@gmail.com>
2022-04-26 14:48:05 +0200

    add link to home on logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import { Menu } from "../index.js";
 import Style from "./Header.module.scss";
 import logo from "../../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={Style.header}>
       <div className={Style.logoContainer}>
-        <img src={logo} alt="Logo de Kasa" />
+        <NavLink to="/">
+          <img src={logo} alt="Logo de Kasa" />
+        </NavLink>
       </div>
       <Menu />
     </header>
